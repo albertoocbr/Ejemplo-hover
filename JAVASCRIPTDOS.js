3ccfd5de2f1c4fd7b2cc6be8367ad630b622f9a7
Alberto.github.io <albertocabreravillarrubia@gmail.com>
2023-01-13 10:10:25 +0100

    pub
    publicar

---
+++ JAVASCRIPTDOS.js
@@ -0,0 +1,12 @@
+    function encender() {
+            document.getElementById('bombilla').style.backgroundColor = 'yellow';
+            document.getElementById('doblecolumna').style.backgroundColor = 'red';
+            document.getElementById('columnauno').style.backgroundColor = 'brown';
+             document.getElementById('columnados').style.backgroundColor = 'purple';
+        }
+         function apagar() {
+            document.getElementById('bombilla').style.backgroundColor = 'black';
+            document.getElementById('columnauno').style.backgroundColor = 'green';
+              document.getElementById('columnados').style.backgroundColor = 'orange';
+            document.getElementById('doblecolumna').style.backgroundColor = 'yellow';
+        }
